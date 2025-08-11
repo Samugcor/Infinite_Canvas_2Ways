1aa4eb7f1989001f9f3952ad5d4363177dc1e0c0
Samugcor <samueligcorp@gmail.com>
2025-08-11 13:26:45 +0200

    feat: zooming on both canvases

--- src/App.jsx
+++ src/App.jsx
@@ -26,18 +26,30 @@ function App() {
   * After your component is removed from the DOM, React will run your cleanup function. 
   * */
   useEffect(()=>{
+    // los guardamos en variables porque el DOM ya no está disponible a la hora de desmontar los listeners
+    const canvas1 = canvas1Ref.current;
+    const canvas2 = canvas2Ref.current; 
     //Necesitamos que los canvas esten cargados en el DOM antes de pedir su contexto.
     ctx1.current = canvas1Ref.current.getContext('2d');
-    ctx2.current = canvas2Ref.current.getContext('2d'); 
+    ctx2.current = canvas2Ref.current.getContext('2d');
+    
+    //Tenemos que usar esta forma de listener porque los que usa react de normal son pasivos, y no nos dejan cancelar el comportamiento predeterminado.
+    canvas1Ref.current.addEventListener("wheel", handleMouseWheel, {passive: false});
+    canvas2Ref.current.addEventListener("wheel", handleMouseWheel, {passive: false});
 
     render1();
     render2(ctx2.current);
 
+    //La función que limpia cuando se desmonta el componente, aquí deberemos eliminar los listeners que hayamos puesto para que no se dupliquen si se vuelve a montar el componente.
+    return () => {
+      canvas1.removeEventListener("wheel", handleMouseWheel, {passive: false});
+      canvas2.removeEventListener("wheel", handleMouseWheel, {passive: false});
+    };
+
   },[]);
 
   
-  
-
+  //Canvas methods
   const drawRectangle = (ctx, x, y, width, height, color) => {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, width, height)
@@ -61,18 +73,18 @@ function App() {
   }
 
   const render2 = (ctx) =>{
-
+    const vt = viewPort2.current;
     clearCanvas(ctx);
-    drawRectangle(ctx, toVirtualX(0), toVirtualY(0), 100, 100, 'red');
-    drawRectangle(ctx, toVirtualX(200), toVirtualY(200), 100, 100, 'blue');
+    drawRectangle(ctx, toVirtualX(0), toVirtualY(0), 100 * vt.scale, 100 * vt.scale, 'red');
+    drawRectangle(ctx, toVirtualX(200), toVirtualY(200), 100 * vt.scale, 100 * vt.scale, 'blue');
   }
 
   //Conversiones
   const toVirtualX = (x)=>{
-    return (x + viewPort2.current.x) * viewPort2.current.scale;
+    return x * viewPort2.current.scale + viewPort2.current.x;
   }
   const toVirtualY = (y)=>{
-    return (y + viewPort2.current.y) * viewPort2.current.scale;
+    return y * viewPort2.current.scale + viewPort2.current.y;
   }
   const toLocalX =(x) => {
     return x / viewPort2.current.scale - viewPort2.current.x;
@@ -80,7 +92,12 @@ function App() {
   const toLocalY =(y) => {
     return y / viewPort2.current.scale - viewPort2.current.y;
   }
-
+  //const toVirtualHeight = () => {
+  //  
+  //}  
+  //const toVirtualHeight = () => {
+  //  
+  //}
 
   //Panning
 
@@ -94,10 +111,23 @@ function App() {
     previousX.current = localX;
     previousY.current = localY;
 
-    
+  }
 
-    //console.log(vt);
-    //console.log(vt.current);
+  //Zooming
+
+  const updateVTZooming = (e, vt, canvas) =>{
+    const rect = canvas.getBoundingClientRect();
+    const localX = e.clientX - rect.left;  
+    const localY = e.clientY - rect.top;
+
+    const newScale = vt.current.scale * (Math.exp(-e.deltaY * 0.001));
+    const newX = localX - (localX - vt.current.x) * (newScale / vt.current.scale);
+    const newY = localY - (localY - vt.current.y) * (newScale / vt.current.scale);
+
+    vt.current.x = newX;
+    vt.current.y = newY;
+    vt.current.scale = newScale;
+
   }
 
   //Eventos
@@ -136,17 +166,37 @@ function App() {
     console.log('mouse is moving')
   }
 
+  const handleMouseWheel = (e) =>{
+    if (e.ctrlKey) {
+      e.preventDefault();
+      if (e.target.id == 'canvas1') {
+        updateVTZooming(e, viewPort1,canvas1Ref.current);
+        render1();
+        setVT1Data({...viewPort1.current});
+      }
+
+      if (e.target.id == 'canvas2') {
+        //console.log("Vamos a actualizar el panning del viewport2")
+        updateVTZooming(e, viewPort2,canvas2Ref.current);
+        render2(ctx2.current);
+        setVT2Data({...viewPort2.current});
+      }
+    }
+
+    //console.log(e);
+  }
+
   return (
     <div className='container'>
       <div className='canvasHolder'>
         <h1>Canvas 1</h1>
         <DataDisplay data = {vT1Data}></DataDisplay>
-        <canvas id='canvas1' ref={canvas1Ref} width={500} height={500} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}></canvas>
+        <canvas id='canvas1' ref={canvas1Ref} width={500} height={500} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} ></canvas>
       </div>
       <div className='canvasHolder'>
         <h1>Canvas 2</h1>
         <DataDisplay data = {vT2Data}></DataDisplay>
-        <canvas id='canvas2' ref={canvas2Ref} width={500} height={500} onMouseDown={handleMouseDown}onMouseUp={handleMouseUp}></canvas>
+        <canvas id='canvas2' ref={canvas2Ref} width={500} height={500} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} ></canvas>
       </div>
     </div>
   )
